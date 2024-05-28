ddb041bd312dc283e9afbf48f2974689432eae52
Ulasatilgan35 <ulasatilgan35@hotmail.com>
2024-05-28 16:17:32 +0300

    Update routes.py

--- backend/server/routes.py
+++ backend/server/routes.py
@@ -6,6 +6,116 @@ from server.models import User, Transaction, Binance_Info, Bot
 from flask_login import login_user, current_user, logout_user, login_required
 import server.trading_bots as trading_bots
 
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/user")
+@login_required
+def user():
+    page = request.args.get('page', 1, type=int)
+    bots = Bot.query.order_by(Bot.id.asc()).paginate(page=page, per_page=5)
+    transactions = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.id.asc()).paginate(page=page, per_page=5)
+    return render_template('user.html', bots=bots, transactions=transactions)
+
+@app.route("/purchase")
+@login_required
+def purchase():
+    page = request.args.get('page', 1, type=int)
+    bots = Bot.query.order_by(Bot.id.asc()).paginate(page=page, per_page=5)
+    transactions = Transaction.query.filter_by(user_id=current_user.id).order_by(Transaction.id.asc()).paginate(page=page, per_page=5)
+    return render_template('purchase.html', bots=bots, transactions=transactions)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        binance_info = Binance_Info(user_id=user.id)
+        db.session.add(binance_info)
+        db.session.commit()
+        flash('Your account has been created successfully.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        user = Binance_Info.query.filter_by(user_id=current_user.id).first()
+        user.api_key = form.api_key.data
+        user.api_secret = form.api_secret.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        user = Binance_Info.query.filter_by(user_id=current_user.id).first()
+        form.api_key.data = user.api_key
+        form.api_secret.data = user.api_secret
+    return render_template('account.html', title='Account', form=form)
+
+@app.route("/transactions/deposit", methods=['GET', 'POST'])
+@login_required
+def transaction_deposit():
+    form = Transaction_DepositForm()
+    if form.validate_on_submit():
+        current_user.balance += form.amount.data
+        db.session.commit()
+        flash('Deposit Successfull.', 'success')
+        return redirect(url_for('user'))
+    return render_template('transaction_deposit.html', form=form)
+
+@app.route("/transactions")
+@app.route("/transactions/")
+def transaction():
+    return redirect(url_for('home'))
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_password():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated.', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', title='Reset Password', form=form)
+
 class give_keys():
     def get_key():
         user = Binance_Info.query.filter_by(user_id=current_user.id).first()
